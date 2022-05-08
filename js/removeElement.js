7724b7a7110416788ce21afc812066c9974afcd9
NYaremiy <n.yaremiy791@gmail.com>
2022-05-08 16:48:54 +0300

    Fixed bug

--- js/removeElement.js
+++ js/removeElement.js
@@ -1,6 +1,7 @@
 import { burger, navMenu, headerIcons, fullScreen } from './variables.js';
 
 const removeEl = () => {
+  document.body.classList.remove('not-scroll');
   burger.classList.remove('c-burger--active');
   navMenu.classList.remove('c-navigation__list--active');
   fullScreen.classList.remove('c-full-screen--show');
--- js/toggleElement.js
+++ js/toggleElement.js
@@ -1,6 +1,7 @@
 import { burger, navMenu, headerIcons, fullScreen } from './variables.js';
 
 const toggleEl = () => {
+  document.body.classList.toggle('not-scroll');
   burger.classList.toggle('c-burger--active');
   navMenu.classList.toggle('c-navigation__list--active');
   fullScreen.classList.toggle('c-full-screen--show');
